ca6bee5ca70aa45e7beedf88d038e195c3fd7aec
David Huber <dave@yomi.eu>
2021-05-26 01:15:22 +0200

    add own interval type

--- model/candle.py
+++ model/candle.py
@@ -1,17 +1,46 @@
-from sqlalchemy import Column, Integer, ForeignKey, String, Float, DateTime, UniqueConstraint
+from sqlalchemy import Column, Integer, ForeignKey, Float, DateTime, UniqueConstraint, Enum
 from sqlalchemy.orm import relationship
+import enum
+from datetime import timedelta
+
 from model._sqlbase import Base
+
+
+@enum.unique
+class Interval(enum.Enum):
+    MINUTE_1 = '1m'
+    MINUTE_5 = '5m'
+    MINUTE_15 = '15m'
+    HOUR_1 = '1h'
+    HOUR_4 = '4h'
+    DAY_1 = '1d'
+    WEEK_1 = '1w'
+
+    def timedelta(self):
+        if self == self.MINUTE_1:
+            return timedelta(minutes=1)
+        elif self == self.MINUTE_5:
+            return timedelta(minutes=5)
+        elif self == self.MINUTE_15:
+            return timedelta(minutes=15)
+        elif self == self.HOUR_1:
+            return timedelta(hours=1)
+        elif self == self.HOUR_4:
+            return timedelta(hours=4)
+        elif self == self.DAY_1:
+            return timedelta(days=1)
+        elif self == self.WEEK_1:
+            return timedelta(weeks=1)
 
 
 class Candle(Base):
     __tablename__ = 'candle'
     __table_args__ = (UniqueConstraint('pair_id', 'interval', 'openTime', name='_candle_unique'),
                       )
-
     id = Column(Integer, primary_key=True)
     pair_id = Column(Integer, ForeignKey('pair.id'))
     pair = relationship("Pair")
-    interval = Column(String)
+    interval = Column(Enum(Interval))
 
     openTime = Column(DateTime)
     closeTime = Column(DateTime)
